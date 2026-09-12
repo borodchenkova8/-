1919306ef768394793f66964678ec5388f6510a5
linia-design <pumochka8908@yandex.ru>
2026-09-12 16:54:30 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,10 @@
 import type { NextConfig } from "next";
 
-const basePath =
-  process.env.BASE_PATH !== undefined ? process.env.BASE_PATH : "";
-
 const nextConfig: NextConfig = {
-  output: process.env.OUTPUT_MODE === "export" ? "export" : undefined,
-
-  ...(basePath && {
-    basePath,
-    assetPrefix: basePath,
-  }),
-
-  ...(process.env.OUTPUT_MODE === "export" && {
-    images: {
-      unoptimized: true,
-    },
-  }),
-  allowedDevOrigins: ["*"],
-
-  devIndicators: false,
-  poweredByHeader: false,
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
