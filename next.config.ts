56fd52b6da50ec7114b9cd532156f1a64eca857a
linia-design <pumochka8908@yandex.ru>
2026-09-12 17:08:40 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
+
